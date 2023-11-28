fe508e2984968f85d57e64cd469af431c2db7dff
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-11-28 15:42:34 +0100

    Add reading of patient record

---
+++ recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/datamodel/PatientRecord.kt
@@ -0,0 +1,13 @@
+package com.hartwig.actin.analysis.recruitment.datamodel
+
+data class PatientRecord(
+    val id : Int,
+    val age : Int,
+    val ecog : Int,
+    val metastaticSites : Set<String>,
+    val pretreatedWithPaclitaxel : Boolean,
+    val psa : Int,
+    val genesInactivated : Set<String>,
+    val treatmentChoice : String,
+    val pfs : Int
+)
---
+++ recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/serialization/PatientRecordReader.kt
@@ -0,0 +1,45 @@
+package com.hartwig.actin.analysis.recruitment.serialization
+
+import com.hartwig.actin.analysis.recruitment.datamodel.PatientRecord
+import java.io.File
+import java.nio.file.Files
+import java.util.HashMap
+
+object PatientRecordReader {
+
+    private const val FIELD_DELIMITER = "\t"
+    private const val VALUE_DELIMITER = ","
+
+    fun read(tsv: String): List<PatientRecord> {
+        val lines = Files.readAllLines(File(tsv).toPath())
+        val fields = createFields(lines[0].split(FIELD_DELIMITER).dropLastWhile { it.isEmpty() }.toTypedArray())
+
+        return lines.subList(1, lines.size).map { createRecord(fields, it.split(FIELD_DELIMITER).toTypedArray()) }
+    }
+
+    private fun createRecord(fields: Map<String, Int>, parts : Array<String>) : PatientRecord {
+        return PatientRecord(
+            id = parts[fields["id"]!!].toInt(),
+            age = parts[fields["age"]!!].toInt(),
+            ecog = parts[fields["ecog"]!!].toInt(),
+            metastaticSites = toSet(parts[fields["metastaticSites"]!!]),
+            pretreatedWithPaclitaxel = parts[fields["pretreatedWithPaclitaxel"]!!] == "1",
+            psa = parts[fields["psa"]!!].toInt(),
+            genesInactivated = toSet(parts[fields["genesInactivated"]!!]),
+            treatmentChoice = parts[fields["treatmentChoice"]!!],
+            pfs = parts[fields["pfs"]!!].toInt()
+        )
+    }
+
+    private fun createFields(header: Array<String>): Map<String, Int> {
+        val fields: MutableMap<String, Int> = HashMap()
+        for ((i) in header.withIndex()) {
+            fields[header[i]] = i
+        }
+        return fields
+    }
+
+    private fun toSet(string : String) : Set<String> {
+        return string.split(VALUE_DELIMITER.toRegex()).toCollection(HashSet())
+    }
+}
