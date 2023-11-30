ba4ea22d44d14d68ae06fda4035c9efacecd6d42
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-11-30 10:16:34 +0100

    Add correct reading of patient db and print patient db

--- recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/PatientLikeMeApplication.kt
+++ recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/PatientLikeMeApplication.kt
@@ -9,11 +9,14 @@ class PatientLikeMeApplication {
     fun run() {
         LOGGER.info("Running patient-like-me application for recruitment assignment")
 
-        val patientDatabase = this.javaClass::class.java.getResource("/database/patients.tsv")!!.path
+        val patientDatabase = this::class.java.classLoader.getResource("database/patients.tsv")!!.path
         LOGGER.info("Reading database from $patientDatabase")
 
         val patients = PatientRecordReader.read(patientDatabase)
-
+        LOGGER.info("Printing patient data")
+        for (patient in patients) {
+            LOGGER.info(" $patient")
+        }
     }
 
     companion object {
@@ -21,6 +24,6 @@ class PatientLikeMeApplication {
     }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     PatientLikeMeApplication().run()
 }
