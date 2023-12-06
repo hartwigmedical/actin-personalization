0518038c8ab4f17d7191828dea81d17339d2cd06
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-12-06 16:40:54 +0100

    Implement a super simple kNN algo

--- recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/algo/NearestNeighborModel.kt
+++ recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/algo/NearestNeighborModel.kt
@@ -1,18 +1,50 @@
 package com.hartwig.actin.analysis.recruitment.algo
 
 import com.hartwig.actin.analysis.recruitment.datamodel.PatientRecord
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import weka.classifiers.lazy.IBk
 import weka.core.Attribute
+import weka.core.DenseInstance
 import weka.core.Instances
 
 object NearestNeighborModel {
 
-    fun run(patients : List<PatientRecord>) {
-        val attributes : ArrayList<Attribute> = ArrayList()
+    private val LOGGER: Logger = LogManager.getLogger(NearestNeighborModel::class)
 
-        attributes.add(Attribute("age"))
-        attributes.add(Attribute("ecog"))
-        attributes.add(Attribute("metastatic sites", listOf("x", "y")))
+    fun run(patients: List<PatientRecord>) {
+        val ageAttr = Attribute("age")
+        val ecogAttr = Attribute("ecog")
+        val pfsAttr = Attribute("pfs")
 
-        val instances = Instances("patients", attributes, 10)
+        val attributes: ArrayList<Attribute> = ArrayList(listOf(ageAttr, ecogAttr, pfsAttr))
+
+        val patientDb = Instances("patients", attributes, patients.size)
+        patientDb.setClassIndex(pfsAttr.index())
+
+        LOGGER.info(" Creating WEKA instances object for ${patients.size} patients")
+        for (patient in patients) {
+            val patientInstance = DenseInstance(attributes.size)
+            patientInstance.setDataset(patientDb)
+            patientInstance.setValue(ageAttr, patient.age.toDouble())
+            patientInstance.setValue(ecogAttr, patient.ecog.toDouble())
+            patientInstance.setClassValue(patient.pfs.toDouble())
+
+            patientDb.add(patientInstance)
+        }
+
+        val k = 1
+        LOGGER.info(" Building K-nearest neighbour model with K=$k")
+        val classifier = IBk(k)
+        classifier.buildClassifier(patientDb)
+
+        val newPatient = DenseInstance(attributes.size)
+        newPatient.setDataset(patientDb)
+        newPatient.setValue(ageAttr, 50.0)
+        newPatient.setValue(ecogAttr, 1.0)
+
+        LOGGER.info(" Classifying new patient with age 50 and ECOG 1")
+        val expectedPfs = classifier.classifyInstance(newPatient)
+        LOGGER.info(" PFS estimated to be $expectedPfs")
     }
 }
