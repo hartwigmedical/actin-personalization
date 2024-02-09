01428add72dce9b77e16b74251bca706f777a06c
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-02-09 10:42:38 +0100

    ACTIN-581: Add a linear regression model

---
+++ recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/LinearRegressionModelApplication.kt
@@ -0,0 +1,33 @@
+package com.hartwig.actin.analysis.recruitment
+
+import com.hartwig.actin.analysis.recruitment.algo.LinearRegressionModel
+import com.hartwig.actin.analysis.recruitment.serialization.PatientRecordReader
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+class LinearRegressionModelApplication {
+
+    fun run() {
+        LOGGER.info("Running linear model application for recruitment assignment")
+
+        val patientDatabasePath = this::class.java.classLoader.getResource("database/patients.tsv")!!.path
+        LOGGER.info("Reading database from $patientDatabasePath")
+
+        val patients = PatientRecordReader.read(patientDatabasePath)
+        LOGGER.info("Printing patient data")
+        for (patient in patients) {
+            LOGGER.info(" $patient")
+        }
+
+        LOGGER.info("Running linear regression")
+        LinearRegressionModel.run(patients)
+    }
+
+    companion object {
+        val LOGGER: Logger = LogManager.getLogger(LinearRegressionModelApplication::class)
+    }
+}
+
+fun main() {
+    LinearRegressionModelApplication().run()
+}
---
+++ recruitment-assignment/src/main/kotlin/com/hartwig/actin/analysis/recruitment/algo/LinearRegressionModel.kt
@@ -0,0 +1,44 @@
+package com.hartwig.actin.analysis.recruitment.algo
+
+import com.hartwig.actin.analysis.recruitment.datamodel.PatientRecord
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import weka.classifiers.functions.LinearRegression
+import weka.core.Attribute
+import weka.core.DenseInstance
+import weka.core.Instances
+
+object LinearRegressionModel {
+
+    private val LOGGER: Logger = LogManager.getLogger(LinearRegression::class)
+
+    fun run(patients: List<PatientRecord>) {
+        val ageAttr = Attribute("age")
+        val psaAttr = Attribute("psa")
+        val ecogAttr = Attribute("ecog")
+        val pfsAttr = Attribute("pfs")
+
+        val attributes: ArrayList<Attribute> = ArrayList(listOf(ageAttr, psaAttr, ecogAttr, pfsAttr))
+
+        val patientDb = Instances("patients", attributes, patients.size)
+        patientDb.setClassIndex(pfsAttr.index())
+
+        LOGGER.info(" Creating WEKA instances object for ${patients.size} patients")
+        for (patient in patients) {
+            val patientInstance = DenseInstance(attributes.size)
+            patientInstance.setDataset(patientDb)
+            patientInstance.setValue(ageAttr, patient.age.toDouble())
+            patientInstance.setValue(psaAttr, patient.psa.toDouble())
+            patientInstance.setValue(ecogAttr, patient.ecog.toDouble())
+            patientInstance.setClassValue(patient.pfs.toDouble())
+
+            patientDb.add(patientInstance)
+        }
+
+        LOGGER.info(" Building linear regression model using to predict ${patientDb.classAttribute().name()}")
+        val linearRegressionModel = LinearRegression()
+        linearRegressionModel.buildClassifier(patientDb)
+
+        LOGGER.info(" Classifier output is $linearRegressionModel")
+    }
+}
