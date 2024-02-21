d96ee5a8f00e4385633e1352b8459448220dfb81
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-02-21 14:02:25 +0100

    ACTIN-542: Take measurementDone into account

--- scripts/cpct_data_analysis.R
+++ scripts/cpct_data_analysis.R
@@ -36,7 +36,7 @@ left join treatment on treatment.biopsyId = biopsy.id
 left join ( select *
             from
             treatmentResponse as tr
-            where response = 'PD' and
+            where response = 'PD' and measurementDone= 'Yes'
             not exists (
               select *
               from treatmentResponse as tr1
