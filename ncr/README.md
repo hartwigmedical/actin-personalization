19192024a70225cec80247f53a3d3a7d41dd034f
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-03-21 15:38:58 +0100

    ACTIN-758: Plumbing for new module

---
+++ ncr/README.md
@@ -0,0 +1,3 @@
+## NCR
+
+This module contains the datamodel mapping for data that was shared via the [NCR](https://iknl.nl/en/ncr) (Netherlands Cancer Registry)
---
+++ ncr/src/main/kotlin/com/hartwig/actin/analysis/ncr/NCRInspectionApplication.kt
@@ -0,0 +1,20 @@
+package com.hartwig.actin.analysis.ncr
+
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+class NCRInspectionApplication {
+
+    fun run() {
+        LOGGER.info("Running NCR inspection application")
+    }
+
+    companion object {
+        val LOGGER: Logger = LogManager.getLogger(NCRInspectionApplication::class)
+    }
+}
+
+fun main() {
+    NCRInspectionApplication().run()
+}
+
