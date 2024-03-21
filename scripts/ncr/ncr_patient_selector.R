b31bc6796e6d3ad3c12e85fab9130d8e18dabae7
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-03-21 09:15:46 +0100

    ACTIN-669: Reuse functions

--- scripts/ncr/ncr_patient_selector.R
+++ scripts/ncr/ncr_patient_selector.R
@@ -2,6 +2,7 @@ library(dplyr)
 
 rm(list = ls())
 source(paste0(Sys.getenv("HOME"), "/hmf/repos/actin-analysis/scripts/ncr/ncr_data_exploration_functions.R"))
+source(paste0(Sys.getenv("HOME"), "/hmf/repos/actin-analysis/scripts/ncr/ncr_patients_like_me_functions.R"))
 
 ncr <- load_ncr_data()
 
@@ -9,21 +10,7 @@ ncr <- load_ncr_data()
 patient_age <- 71
 patient_who <- 0
 
-## Rules:
-##  - Age within one year of patient and matching WHO status (perf_stat)
-##  - DIA episodes where metastases where present at start of episode
-##  - No prior malignancies (mal1_int NA)
-##  - Adenocarcinoma (morf_cat == 1) without any other tumors (dubbeltum == 0)
-##  - Has had treatment (tumgericht_ther == 1)
-ncr_matches <- ncr %>% dplyr::filter(epis == 'DIA' &
-                                       meta_epis == 1 &
-                                       is.na(mal1_int) &
-                                       morf_cat == 1 &
-                                       dubbeltum == 0 &
-                                       tumgericht_ther == 1 &
-                                       leeft >= patient_age - 1 &
-                                       leeft <= patient_age + 1 &
-                                       perf_stat == patient_who)
+ncr_matches <- find_similar_patients(ncr, patient_age, patient_who)
 
 ncr_matches %>% summarise(count_key_nkr = n(), distinct_count_key_nkr = n_distinct(key_nkr))
 
