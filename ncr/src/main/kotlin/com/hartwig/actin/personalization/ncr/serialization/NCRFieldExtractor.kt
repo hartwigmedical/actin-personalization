05e8a1227c0c1876354c6b18592a92501edb077e
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-03-22 17:12:01 +0100

    ACTIN-758: Add (raw) NCR datamodel (WIP)

---
+++ ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/serialization/NCRFieldExtractor.kt
@@ -0,0 +1,20 @@
+package com.hartwig.actin.personalization.ncr.serialization
+
+class NCRFieldExtractor(private val fields: Map<String, Int>, private val parts: Array<String>) {
+
+    fun mandatoryInt(property : String) : Int {
+        return value(property).toInt()
+    }
+
+    fun optionalInt(property : String) : Int? {
+        return value(property).ifBlank { null }?.toInt()
+    }
+
+    fun optionalDouble(property : String) : Double? {
+        return value(property).ifBlank { null }?.toDouble()
+    }
+
+    private fun value(property : String) : String {
+        return parts[fields[property]!!]
+    }
+}
