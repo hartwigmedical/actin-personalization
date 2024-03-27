4bd4043ad93b97d132e3678e093e07ba937d9cf3
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-03-27 11:23:46 +0100

    ACTIN-758: Add various initial stats

--- ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/NCRInspectionApplication.kt
+++ ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/NCRInspectionApplication.kt
@@ -1,6 +1,7 @@
 package com.hartwig.actin.personalization.ncr
 
 import com.hartwig.actin.personalization.ncr.serialization.NCRDataReader
+import com.hartwig.actin.personalization.ncr.serialization.datamodel.NCRRecord
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
@@ -13,6 +14,29 @@ class NCRInspectionApplication {
         val ncrRecords = NCRDataReader.read(ncrDataPath)
 
         LOGGER.info(" Read {} NCR records from {}", ncrRecords.size, ncrDataPath)
+
+        LOGGER.info("General statistics related to NCR database")
+        LOGGER.info(" {} unique patients found, with {} tumors and {} episodes",
+            patientCount(ncrRecords), tumorCount(ncrRecords), episodeCount(ncrRecords))
+        LOGGER.info(" {} patients are still alive at the time of data release", alivePatients(ncrRecords))
+
+        LOGGER.info("Done!")
+    }
+
+    private fun patientCount(ncrRecords: List<NCRRecord>): Int {
+        return ncrRecords.map { it.identification.keyNkr }.distinct().count()
+    }
+
+    private fun tumorCount(ncrRecords: List<NCRRecord>): Int {
+        return ncrRecords.map { it.identification.keyZid }.distinct().count()
+    }
+
+    private fun episodeCount(ncrRecords: List<NCRRecord>): Int {
+        return ncrRecords.map { it.identification.keyEid }.distinct().count()
+    }
+
+    private fun alivePatients(ncrRecords: List<NCRRecord>): Any {
+        return patientCount(ncrRecords.filter { it.patientCharacteristics.vitStat == 0 })
     }
 
     companion object {
