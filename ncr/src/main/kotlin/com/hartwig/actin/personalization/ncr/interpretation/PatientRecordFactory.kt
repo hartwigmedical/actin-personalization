e36efb67329928eaf5c829306aeb02130bafda64
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-03-29 16:33:30 +0100

    ACTIN-758: Initial implementation of patient record creation (WIP)

--- ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/PatientRecordFactory.kt
+++ ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/PatientRecordFactory.kt
@@ -1,15 +1,30 @@
 package com.hartwig.actin.personalization.ncr.interpretation
 
 import com.hartwig.actin.personalization.ncr.datamodel.PatientRecord
+import com.hartwig.actin.personalization.ncr.datamodel.Sex
+import com.hartwig.actin.personalization.ncr.datamodel.VitalStatus
 import com.hartwig.actin.personalization.ncr.serialization.datamodel.NCRRecord
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
 
 object PatientRecordFactory {
 
-    fun create(ncrRecords : List<NCRRecord>) : List<PatientRecord> {
-        return listOf()
+    fun create(ncrRecords: List<NCRRecord>): List<PatientRecord> {
+        val recordsPerPatient: Map<Int, List<NCRRecord>> = ncrRecords.groupBy { it.identification.keyNkr }
+
+        return recordsPerPatient.entries.map { createPatientRecord(it.value) }
     }
 
-    private val LOGGER: Logger = LogManager.getLogger(PatientRecordFactory::class)
+    private fun createPatientRecord(ncrRecords: List<NCRRecord>): PatientRecord {
+        val patientIDs : List<Int> = ncrRecords.map { it.identification.keyNkr }.distinct()
+        if (patientIDs.count() != 1) {
+            throw IllegalStateException("Non-unique or missing patientID when creating a single patient record: $patientIDs")
+        }
+
+        return PatientRecord(
+            id = patientIDs[0],
+            sex = Sex.MALE,
+            vitalStatus = VitalStatus.ALIVE,
+            episodesPerTumorOfInterest = mapOf(),
+            previousTumors = listOf()
+        )
+    }
 }
