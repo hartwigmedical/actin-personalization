e8decbdf09ecf52872e0b670eeed1fcee8cefca8
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-04-17 14:54:30 +0200

    ACTIN-758: Add a mapping

--- ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/NcrCodeResolver.kt
+++ ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/NcrCodeResolver.kt
@@ -1,51 +1,7 @@
 package com.hartwig.actin.personalization.ncr.interpretation
 
-import com.hartwig.actin.personalization.datamodel.AnastomoticLeakageAfterSurgery
-import com.hartwig.actin.personalization.datamodel.AnorectalVergeDistanceCategory
-import com.hartwig.actin.personalization.datamodel.AsaClassificationPreSurgeryOrEndoscopy
-import com.hartwig.actin.personalization.datamodel.CciNumberOfCategories
-import com.hartwig.actin.personalization.datamodel.DistantMetastasesStatus
-import com.hartwig.actin.personalization.datamodel.ExtraMuralInvasionCategory
-import com.hartwig.actin.personalization.datamodel.GastroenterologyResectionType
-import com.hartwig.actin.personalization.datamodel.LymphaticInvasionCategory
-import com.hartwig.actin.personalization.datamodel.PFSMeasureType
-import com.hartwig.actin.personalization.datamodel.RadiotherapyType
-import com.hartwig.actin.personalization.datamodel.Sex
-import com.hartwig.actin.personalization.datamodel.SurgeryCircumferentialResectionMargin
-import com.hartwig.actin.personalization.datamodel.SurgeryRadicality
-import com.hartwig.actin.personalization.datamodel.SurgeryTechnique
-import com.hartwig.actin.personalization.datamodel.SurgeryType
-import com.hartwig.actin.personalization.datamodel.SurgeryUrgency
-import com.hartwig.actin.personalization.datamodel.TumorBasisOfDiagnosis
-import com.hartwig.actin.personalization.datamodel.TumorDifferentiationGrade
-import com.hartwig.actin.personalization.datamodel.TumorLocationCategory
-import com.hartwig.actin.personalization.datamodel.TumorRegression
-import com.hartwig.actin.personalization.datamodel.TumorType
-import com.hartwig.actin.personalization.datamodel.VenousInvasionCategory
-import com.hartwig.actin.personalization.datamodel.VitalStatus
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrAnastomoticLeakageAfterSurgeryMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrAnorectalVergeDistanceCategoryMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrAsaClassificationPreSurgeryOrEndoscopyMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrCciNumberOfCategoriesMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrDistantMetastasesStatusMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrExtraMuralInvasionCategoryMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrGastroenterologyResectionTypeMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrLymphaticInvasionCategoryMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrPFSMeasureTypeMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrRadiotherapyTypeMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrSexMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrSurgeryCircumferentialResectionMarginMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrSurgeryRadicalityMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrSurgeryTechniqueMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrSurgeryTypeMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrSurgeryUrgencyMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrTumorBasisOfDiagnosisMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrTumorDifferentiationGradeMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrTumorLocationCategoryMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrTumorRegressionMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrTumorTypeMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrVenousInvasionCategoryMapper
-import com.hartwig.actin.personalization.ncr.interpretation.mapper.NcrVitalStatusMapper
+import com.hartwig.actin.personalization.datamodel.*
+import com.hartwig.actin.personalization.ncr.interpretation.mapper.*
 
 object NcrCodeResolver {
 
@@ -61,6 +17,7 @@ object NcrCodeResolver {
             LymphaticInvasionCategory::class -> NcrLymphaticInvasionCategoryMapper
             PFSMeasureType::class -> NcrPFSMeasureTypeMapper
             RadiotherapyType::class -> NcrRadiotherapyTypeMapper
+            ReasonRefrainmentFromTumorDirectedTreatment::class -> NcrReasonRefrainmentFromTumorDirectedTherapyMapper
             Sex::class -> NcrSexMapper
             SurgeryCircumferentialResectionMargin::class -> NcrSurgeryCircumferentialResectionMarginMapper
             SurgeryRadicality::class -> NcrSurgeryRadicalityMapper
