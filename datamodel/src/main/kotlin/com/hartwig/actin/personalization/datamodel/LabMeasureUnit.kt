01fe68d36bda17b9359c3161e8d7fbf57b21509a
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-04-17 14:41:55 +0200

    ACTIN-758: Some ugly sortings

--- datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/LabMeasureUnit.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/LabMeasureUnit.kt
@@ -1,8 +1,8 @@
 package com.hartwig.actin.personalization.datamodel
 
 enum class LabMeasureUnit {
+    BILLIONS_PER_LITER,
+    GRAM_PER_LITER,
     MICROGRAM_PER_LITER,
     UNIT_PER_LITER,
-    GRAM_PER_LITER,
-    BILLIONS_PER_LITER,
 }
