6078264b299ac9a9d1df9548349b1ce96ee5fc65
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-05-02 15:12:53 -0400

    ACTIN-933: Add TreatmentPlan to datamodel

---
+++ datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/TreatmentPlan.kt
@@ -0,0 +1,30 @@
+package com.hartwig.actin.personalization.datamodel
+
+enum class TreatmentPlan(val drugs: Set<Drug>?) {
+    CAPOX(setOf(Drug.CAPECITABINE, Drug.OXALIPLATIN)),
+    CAPOX_B(setOf(Drug.CAPECITABINE, Drug.OXALIPLATIN, Drug.BEVACIZUMAB)),
+    CAPECITABINE(setOf(Drug.CAPECITABINE)),
+    CAPECITABINE_BEVACIZUMAB(setOf(Drug.CAPECITABINE, Drug.BEVACIZUMAB)),
+    FOLFIRI(setOf(Drug.FLUOROURACIL, Drug.IRINOTECAN)),
+    FOLFIRI_B(setOf(Drug.FLUOROURACIL, Drug.IRINOTECAN, Drug.BEVACIZUMAB)),
+    FOLFIRI_P(setOf(Drug.FLUOROURACIL, Drug.IRINOTECAN, Drug.PANITUMUMAB)),
+    FOLFOX(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN)),
+    FOLFOX_B(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.BEVACIZUMAB)),
+    FOLFOX_P(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.PANITUMUMAB)),
+    FOLFOXIRI(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.IRINOTECAN)),
+    FOLFOXIRI_B(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.IRINOTECAN, Drug.BEVACIZUMAB)),
+    FLUOROURACIL(setOf(Drug.FLUOROURACIL)),
+    FLUOROURACIL_BEVACIZUMAB(setOf(Drug.FLUOROURACIL, Drug.BEVACIZUMAB)),
+    IRINOTECAN(setOf(Drug.IRINOTECAN)),
+    NIVOLUMAB(setOf(Drug.NIVOLUMAB)),
+    OTHER(null),
+    PEMBROLIZUMAB(setOf(Drug.PEMBROLIZUMAB));
+
+    companion object {
+        private val plansByDrugSet = values().associateBy(TreatmentPlan::drugs)
+
+        fun findForDrugs(drugs: Set<Drug>?): TreatmentPlan {
+            return plansByDrugSet[drugs] ?: OTHER
+        }
+    }
+}
