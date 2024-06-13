5bcadaed344df22216404825ee9a48059bf5406c
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-06-13 13:39:36 +0200

    ACTIN-1021: Correction

--- datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/SystemicTreatmentPlan.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/SystemicTreatmentPlan.kt
@@ -6,7 +6,7 @@ data class SystemicTreatmentPlan( //Should be based on combination of all treatm
     val intervalTumorIncidenceTreatmentPlanStop: Int?, //intervalTumorIncidenceTreatmentLineStopMax of last scheme nr
 
     val treatmentPlanResponses: List<ResponseMeasure>, //All responses of the episode
-    val treatmentPlanBestResponse: ResponseMeasure?, //CR>PR>MR>SD>PD
+    val treatmentPlanBestResponse: ResponseMeasureType?, //CR>PR>MR>SD>PD
     val treatmentPlanPfsValues: List<PfsMeasure>, //All pfs measure entries
     val treatmentPlanPfs: Int?, // Calculate as minimal (intervalTumorIncidencePfsMeasureDate-intervalTumorIncidenceTreatmentPlanStart) from all available PFS measures of Type PROGRESSION or DEATH (so ignore CENSOR for now), and any FollowUpEvent.
 )
