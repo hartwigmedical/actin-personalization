a14c4729b1de4071388794c4ff98be29edaa409f
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-06-13 09:18:33 +0200

    ACTIN-1021: Proposed changes to data model

--- datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/SystemicTreatmentPlan.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/SystemicTreatmentPlan.kt
@@ -1,30 +1,10 @@
 package com.hartwig.actin.personalization.datamodel
 
-enum class SystemicTreatmentPlan(val drugs: Set<Drug>?) {
-    CAPOX(setOf(Drug.CAPECITABINE, Drug.OXALIPLATIN)),
-    CAPOX_B(setOf(Drug.CAPECITABINE, Drug.OXALIPLATIN, Drug.BEVACIZUMAB)),
-    CAPECITABINE(setOf(Drug.CAPECITABINE)),
-    CAPECITABINE_BEVACIZUMAB(setOf(Drug.CAPECITABINE, Drug.BEVACIZUMAB)),
-    FOLFIRI(setOf(Drug.FLUOROURACIL, Drug.IRINOTECAN)),
-    FOLFIRI_B(setOf(Drug.FLUOROURACIL, Drug.IRINOTECAN, Drug.BEVACIZUMAB)),
-    FOLFIRI_P(setOf(Drug.FLUOROURACIL, Drug.IRINOTECAN, Drug.PANITUMUMAB)),
-    FOLFOX(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN)),
-    FOLFOX_B(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.BEVACIZUMAB)),
-    FOLFOX_P(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.PANITUMUMAB)),
-    FOLFOXIRI(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.IRINOTECAN)),
-    FOLFOXIRI_B(setOf(Drug.FLUOROURACIL, Drug.OXALIPLATIN, Drug.IRINOTECAN, Drug.BEVACIZUMAB)),
-    FLUOROURACIL(setOf(Drug.FLUOROURACIL)),
-    FLUOROURACIL_BEVACIZUMAB(setOf(Drug.FLUOROURACIL, Drug.BEVACIZUMAB)),
-    IRINOTECAN(setOf(Drug.IRINOTECAN)),
-    NIVOLUMAB(setOf(Drug.NIVOLUMAB)),
-    OTHER(null),
-    PEMBROLIZUMAB(setOf(Drug.PEMBROLIZUMAB));
-
-    companion object {
-        private val plansByDrugSet = values().associateBy(SystemicTreatmentPlan::drugs)
-
-        fun findForDrugs(drugs: Set<Drug>?): SystemicTreatmentPlan {
-            return plansByDrugSet[drugs] ?: OTHER
-        }
-    }
-}
+data class SystemicTreatmentPlan( //Should be based on combination of all treatment schemes of same episode
+    val treatmentPlan: SystemicTreatmentPlanName,
+    val intervalTumorIncidenceTreatmentPlanStart: Int?, //intervalTumorIncidenceTreatmentLineStartMin of scheme nr = 1
+    val intervalTumorIncidenceTreatmentPlanStop: Int?, //intervalTumorIncidenceTreatmentLineStopMax of last scheme nr
+    val treatmentPlanResponses: List<ResponseMeasure>, //All responses of the episode
+    val treatmentPlanPfsValues: List<PfsMeasure>, //All pfs measure entries
+    val treatmentPlanPfs: PfsMeasureType?, //Calculate as max pfs value of type PROGRESSION/DEATH
+)
--- datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/SystemicTreatmentScheme.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/SystemicTreatmentScheme.kt
@@ -7,17 +7,4 @@ data class SystemicTreatmentScheme( //Should combine treatments of same scheme n
     val intervalTumorIncidenceTreatmentLineStopMin: Int?,
     val intervalTumorIncidenceTreatmentLineStopMax: Int?,
 
-    //Should be added when episode response measure belongs to this treatment (i.e. should be prior to min start of following systemic treatment line)
-    val treatmentResponse: ResponseMeasure?,
-
-    val treatmentPfs: PfsMeasureType?, //Max pfs measure of type PROGRESSION/DEATH belongs to this treatment (i.e. should be measured prior to min start of following systemic treatment line)
-    val treatmentRawPfs: List<PfsMeasure> //All pfs measure entries belonging to this treatment (i.e. should be measured prior to min start of following systemic treatment line)
-) {
-
-    fun intervalTreatmentStartMinResponseDate(): Int? = intervalTumorIncidenceTreatmentLineStartMin?.let(::responseInterval)
-
-    fun intervalTreatmentStartMaxResponseDate(): Int? = intervalTumorIncidenceTreatmentLineStartMax?.let(::responseInterval)
-
-    private fun responseInterval(referenceInterval: Int): Int? =
-        treatmentResponse?.intervalTumorIncidenceResponseDate?.minus(referenceInterval)
-}
+)
