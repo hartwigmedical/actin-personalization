d058f9200fea5e8899c0f3ea644de8afdd3b3cfc
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-09-11 11:50:21 +0200

    ACTIN-1275: PR feedback

--- ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/mapper/NcrTnmTMapper.kt
+++ ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/mapper/NcrTnmTMapper.kt
@@ -3,7 +3,7 @@ package com.hartwig.actin.personalization.ncr.interpretation.mapper
 import com.hartwig.actin.personalization.datamodel.TnmT
 
 object NcrTnmTMapper : NcrStringCodeMapper<TnmT?> {
-    override fun resolve(code: String): TnmT? {
+    override fun resolve(code: String): TnmT {
         return when (code) {
             "0" -> TnmT.T0
             "IS" -> TnmT.T_IS
@@ -13,7 +13,6 @@ object NcrTnmTMapper : NcrStringCodeMapper<TnmT?> {
             "4A" -> TnmT.T4A
             "4B" -> TnmT.T4B
             "X" -> TnmT.X
-            "" -> null
             else -> throw IllegalArgumentException("Unknown TnmT code: $code")
         }
     }
