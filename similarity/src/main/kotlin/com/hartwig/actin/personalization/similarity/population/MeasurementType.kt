7df75f44a14c21a68090c6471870e83d8b85b1c5
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-09-11 16:05:04 -0400

    ACTIN-1262: Drop unused PercentPfsWithDaysCalculation and population tables

--- similarity/src/main/kotlin/com/hartwig/actin/personalization/similarity/population/MeasurementType.kt
+++ similarity/src/main/kotlin/com/hartwig/actin/personalization/similarity/population/MeasurementType.kt
@@ -2,8 +2,5 @@ package com.hartwig.actin.personalization.similarity.population
 
 enum class MeasurementType(val calculation: Calculation) {
     TREATMENT_DECISION(TreatmentDecisionCalculation),
-    PROGRESSION_FREE_SURVIVAL(PfsCalculation),
-    PERCENT_WITH_PFS_THREE_MONTHS(PercentPfsWithDaysCalculation(91)),
-    PERCENT_WITH_PFS_SIX_MONTHS(PercentPfsWithDaysCalculation(183)),
-    PERCENT_WITH_PFS_ONE_YEAR(PercentPfsWithDaysCalculation(365))
+    PROGRESSION_FREE_SURVIVAL(PfsCalculation)
 }
--- similarity/src/test/kotlin/com/hartwig/actin/personalization/similarity/TestPersonalizationReportWriterApplication.kt
+++ similarity/src/test/kotlin/com/hartwig/actin/personalization/similarity/TestPersonalizationReportWriterApplication.kt
@@ -18,19 +18,12 @@ class TestPersonalizationReportWriterApplication {
             .analyzePatient(50, 1, false, emptySet())
 
         val measurementTables = listOf(
-            MeasurementType.TREATMENT_DECISION,
-            MeasurementType.PROGRESSION_FREE_SURVIVAL,
-            MeasurementType.PERCENT_WITH_PFS_THREE_MONTHS,
-            MeasurementType.PERCENT_WITH_PFS_ONE_YEAR
+            MeasurementType.TREATMENT_DECISION, MeasurementType.PROGRESSION_FREE_SURVIVAL
         ).map { TableContent.createForMeasurementType(analysis, it) }
-
-        val populationTables = analysis.populations.map { TableContent.createForPopulation(analysis, it) }
 
         LOGGER.info { "Writing PDF report to $outputPath" }
         val writer = ReportWriter.create(outputPath)
-        writer.writeReport(
-            "SOC personalized real-world evidence annotation", measurementTables + populationTables, analysis.plots
-        )
+        writer.writeReport("SOC personalized real-world evidence annotation", measurementTables, analysis.plots)
 
         LOGGER.info { "Done!" }
         return 0
