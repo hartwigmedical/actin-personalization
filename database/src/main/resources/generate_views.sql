b15b80d1d0f11c7c19f71ea94e4b134a820a5ff8
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-09-24 17:17:13 -0400

    ACTIN-1301: Add columns to view and add filteredView

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -1,51 +1,70 @@
 CREATE OR REPLACE VIEW diagnosisTreatments
 AS (
 SELECT
-    ncrId,
+    p.ncrId,
+    p.sex,
+    p.isAlive,
     d.patientId,
-    e.diagnosisId,
-    e.id AS episodeId,
-    e.order,
-    distantMetastasesDetectionStatus,
-    consolidatedTumorType,
-    tumorLocations,
-    ageAtDiagnosis,
-    whoStatusPreTreatmentStart,
-    hasHadPriorTumor,
-    hasMsi,
-    hasBrafMutation,
-    hasBrafV600EMutation,
-    hasRasMutation,
-    hasKrasG12CMutation,
-    hasReceivedTumorDirectedTreatment,
-    reasonRefrainmentFromTumorDirectedTreatment,
-    gastroenterologyResections,
+    d.consolidatedTumorType,
+    d.tumorLocations,
+    d.ageAtDiagnosis,
+    d.observedOverallSurvivalFromTumorIncidenceDays,
+    d.hadSurvivalEvent,
+    d.hasHadPriorTumor,
+    d.cci,
+    d.cciNumberOfCategories,
+    d.cciHasAids,
+    d.cciHasCongestiveHeartFailure,
+    d.cciHasCollagenosis,
+    d.cciHasCopd,
+    d.cciHasCerebrovascularDisease,
+    d.cciHasDementia,
+    d.cciHasDiabetesMellitus,
+    d.cciHasDiabetesMellitusWithEndOrganDamage,
+    d.cciHasOtherMalignancy,
+    d.cciHasOtherMetastaticSolidTumor,
+    d.cciHasMyocardialInfarct,
+    d.cciHasMildLiverDisease,
+    d.cciHasHemiplegiaOrParaplegia,
+    d.cciHasPeripheralVascularDisease,
+    d.cciHasRenalDisease,
+    d.cciHasLiverDisease,
+    d.cciHasUlcerDisease,
+    d.presentedWithIleus,
+    d.presentedWithPerforation,
+    d.anorectalVergeDistanceCategory,
+    d.hasMsi,
+    d.hasBrafMutation,
+    d.hasBrafV600EMutation,
+    d.hasRasMutation,
+    d.hasKrasG12CMutation,
+    e.*,
 	surgeries,
-    metastasesSurgeries,
-    radiotherapies,
-    metastasesRadiotherapies,
-    hasHadHipecTreatment,
-    hasHadPreSurgerySystemicChemotherapy,
-    hasHadPostSurgerySystemicChemotherapy,
-    hasHadPreSurgerySystemicTargetedTherapy,
-    hasHadPostSurgerySystemicTargetedTherapy,
 	metastasisLocationGroupsPriorToSystemicTreatment,
-    systemicTreatmentPlan,
-    intervalTumorIncidenceTreatmentPlanStartDays,
-    intervalTumorIncidenceTreatmentPlanStopDays,
-    intervalTumorIncidenceTreatmentPlanStopDays-intervalTumorIncidenceTreatmentPlanStartDays AS systemicTreatmentPlanDuration,
-    observedPfsDays AS systemicTreatmentPlanPfs,
-    response,
-    intervalTreatmentPlanStartResponseDays
-FROM
-    patient p
-        INNER JOIN
-    diagnosis d ON p.id = d.patientId
-        INNER JOIN
-    episode e ON d.id = e.diagnosisId
-        LEFT JOIN
-    (SELECT episodeId, GROUP_CONCAT(type) AS surgeries FROM surgery GROUP BY 1) AS s ON e.id = s.episodeId
-		LEFT JOIN
-	(SELECT episodeId, GROUP_CONCAT(locationGroup ORDER BY locationGroup) AS metastasisLocationGroupsPriorToSystemicTreatment FROM metastasis mm INNER JOIN episode ee ON mm.episodeId=ee.id WHERE intervalTumorIncidenceMetastasisDetectionDays < intervalTumorIncidenceTreatmentPlanStartDays GROUP BY 1) AS m ON e.id=m.episodeId
-WHERE e.order=1
+    intervalTumorIncidenceTreatmentPlanStopDays-intervalTumorIncidenceTreatmentPlanStartDays AS systemicTreatmentPlanDuration
+FROM patient p
+JOIN diagnosis d ON p.id = d.patientId
+JOIN episode e ON d.id = e.diagnosisId AND e.order=1
+LEFT JOIN (
+    SELECT episodeId, GROUP_CONCAT(type) AS surgeries FROM surgery GROUP BY id
+) s ON e.id = s.episodeId
+LEFT JOIN (
+    SELECT episodeId, GROUP_CONCAT(locationGroup ORDER BY locationGroup) AS metastasisLocationGroupsPriorToSystemicTreatment
+    FROM metastasis mm
+    JOIN episode ee ON mm.episodeId=ee.id
+    WHERE intervalTumorIncidenceMetastasisDetectionDays < intervalTumorIncidenceTreatmentPlanStartDays
+    GROUP BY episodeId
+) m ON e.id=m.episodeId
+);
+
+CREATE OR REPLACE VIEW filteredTreatments
+AS (
+SELECT *
+FROM diagnosisTreatments
+WHERE distantMetastasesDetectionStatus = 'AT_START'
+AND hasHadPreSurgerySystemicChemotherapy = 0
+AND hasHadPostSurgerySystemicChemotherapy = 0
+AND hasHadPreSurgerySystemicTargetedTherapy = 0
+AND hasHadPostSurgerySystemicTargetedTherapy = 0
+AND surgeries IS NULL
 );
