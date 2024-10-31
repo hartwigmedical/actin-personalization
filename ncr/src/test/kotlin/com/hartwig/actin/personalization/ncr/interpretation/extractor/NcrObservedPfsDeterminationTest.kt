f4be783acf21f8b499fdbf91e7986e3f2b339b98
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-10-31 08:26:46 +0100

    ACTIN-1378: Improve clarity and exhaustiveness of tests, remove superfluous ones

--- ncr/src/test/kotlin/com/hartwig/actin/personalization/ncr/interpretation/extractor/NcrObservedPfsDeterminationTest.kt
+++ ncr/src/test/kotlin/com/hartwig/actin/personalization/ncr/interpretation/extractor/NcrObservedPfsDeterminationTest.kt
@@ -2,15 +2,28 @@ package com.hartwig.actin.personalization.ncr.interpretation.extractor
 
 import com.hartwig.actin.personalization.datamodel.PfsMeasure
 import com.hartwig.actin.personalization.datamodel.PfsMeasureType
+import com.hartwig.actin.personalization.ncr.datamodel.NcrSystemicTreatment
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class NcrObservedPfsDeterminationTest {
+    private val interval = 300
+    private val response = null
+    private val treatment = NcrSystemicTreatment(
+        chemo = 1,
+        target = 1,
+        systCode1 = "L01XA03",
+        systPrepost1 = 1,
+        systSchemanum1 = 1,
+        systKuren1 = 1,
+        systStartInt1 = 5,
+        systStopInt1 = 20
+    )
 
     @Test
     fun `Should ignore pfs measure if measure occurred before treatment plan start`() {
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, listOf(PfsMeasure(PfsMeasureType.PROGRESSION, null, 0)), null, 300)
+        val pfsMeasures = listOf(PfsMeasure(PfsMeasureType.PROGRESSION, null, 4))
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(treatment, pfsMeasures, response, interval)
         assertThat(plan!!.observedPfsDays).isNull()
         assertThat(plan.hadProgressionEvent).isNull()
     }
@@ -19,118 +32,97 @@ class NcrObservedPfsDeterminationTest {
     fun `Should not determine observedPfsDays or hadProgressionEvent in case at least one interval is null`() {
         val pfsMeasures = listOf(
             PfsMeasure(PfsMeasureType.PROGRESSION, null, 5),
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, null),
+            PfsMeasure(PfsMeasureType.CENSOR, null, null),
         )
-
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, pfsMeasures, null, 300)
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(treatment, pfsMeasures, response, interval)
         assertThat(plan!!.observedPfsDays).isNull()
         assertThat(plan.hadProgressionEvent).isNull()
     }
 
     @Test
-    fun `Should interpret censor pfs value if measure occurred after treatment plan start`() {
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, listOf(PfsMeasure(PfsMeasureType.CENSOR, null, 50)), null, 300)
-        assertThat(plan!!.observedPfsDays).isEqualTo(49)
+    fun `Should interpret censor pfs measure correctly if measure occurred after treatment plan start`() {
+        val pfsMeasures = listOf(PfsMeasure(PfsMeasureType.CENSOR, null, 50))
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(treatment, pfsMeasures, response, interval)
+        assertThat(plan!!.observedPfsDays).isEqualTo(45)
         assertThat(plan.hadProgressionEvent).isFalse()
     }
 
     @Test
-    fun `Should interpret progression pfs value if measure occurred after treatment plan start`() {
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, listOf(PfsMeasure(PfsMeasureType.PROGRESSION, null, 50)), null, 300)
-        assertThat(plan!!.observedPfsDays).isEqualTo(49)
+    fun `Should interpret progression pfs value correctly if measure occurred after treatment plan start`() {
+        val pfsMeasures = listOf(PfsMeasure(PfsMeasureType.PROGRESSION, null, 50))
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(treatment, pfsMeasures, response, interval)
+        assertThat(plan!!.observedPfsDays).isEqualTo(45)
         assertThat(plan.hadProgressionEvent).isTrue()
     }
 
     @Test
-    fun `Should interpret second pfs value if one occurred before and one after start in case of n=2 progression measures`() {
+    fun `Should determine observedPfsDays or hadProgressionEvent in case missing stop date if there is only one progression measure`() {
+        val pfsMeasures = listOf(PfsMeasure(PfsMeasureType.PROGRESSION, null, 50))
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(
+            treatment.copy(systStopInt1 = null),
+            pfsMeasures,
+            response,
+            interval
+        )
+        assertThat(plan!!.observedPfsDays).isEqualTo(45)
+        assertThat(plan.hadProgressionEvent).isTrue()
+    }
+
+    @Test
+    fun `Should not determine observedPfsDays or hadProgressionEvent in case missing stop date and there are multiple progression measures`() {
         val pfsMeasures = listOf(
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 5),
             PfsMeasure(PfsMeasureType.PROGRESSION, null, 10),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 50),
         )
-
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, pfsMeasures, null, 300)
-        assertThat(plan!!.observedPfsDays).isEqualTo(9)
-        assertThat(plan.hadProgressionEvent).isTrue()
-    }
-
-    @Test
-    fun `Should interpret second pfs value if both occurred before end date in case of n=2 progression measures`() {
-        val pfsMeasures = listOf(
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 2),
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 4),
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(
+            treatment.copy(systStopInt1 = null),
+            pfsMeasures,
+            response,
+            interval
         )
-
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, pfsMeasures, null, 300)
-        assertThat(plan!!.observedPfsDays).isEqualTo(3)
-        assertThat(plan.hadProgressionEvent).isTrue()
-    }
-
-    @Test
-    fun `Should interpret first pfs value if both occurred after treatment end date in case of n=2 progression measures`() {
-        val pfsMeasures = listOf(
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 12),
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 16),
-        )
-
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, pfsMeasures, null, 300)
-        assertThat(plan!!.observedPfsDays).isEqualTo(11)
-        assertThat(plan.hadProgressionEvent).isTrue()
-    }
-
-    @Test
-    fun `Should interpret first one after treatment plan stop date in case more than 2 progression measures`() {
-        val pfsMeasures = listOf(
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 4),
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 20),
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 21),
-        )
-
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, pfsMeasures, null, 300)
-        assertThat(plan!!.observedPfsDays).isEqualTo(19)
-        assertThat(plan.hadProgressionEvent).isTrue()
-    }
-
-    @Test
-    fun `Should resolve to null in case stop date is null and there are two progression measures`() {
-        val pfsMeasures = listOf(
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 4),
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 20),
-        )
-
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(
-                NCR_SYSTEMIC_TREATMENT.copy(
-                    systStopInt1 = null,
-                    systStopInt2 = null,
-                    systStopInt3 = null,
-                    systStopInt4 = null,
-                    systStopInt5 = null,
-                    systStopInt6 = null,
-                    systStopInt7 = null
-                ), pfsMeasures, null, 300
-            )
         assertThat(plan!!.observedPfsDays).isNull()
         assertThat(plan.hadProgressionEvent).isNull()
     }
 
     @Test
-    fun `Should interpret last pfs measure if all before stop date in case more than 2 progression measures`() {
+    fun `Should interpret first pfs measure after treatment end if there is at least one measure after treatment end`() {
         val pfsMeasures = listOf(
             PfsMeasure(PfsMeasureType.PROGRESSION, null, 2),
-            PfsMeasure(PfsMeasureType.PROGRESSION, null, 4),
             PfsMeasure(PfsMeasureType.PROGRESSION, null, 6),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 22),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 25),
         )
 
-        val plan = NcrSystemicTreatmentPlanExtractor()
-            .extractSystemicTreatmentPlan(NCR_SYSTEMIC_TREATMENT, pfsMeasures, null, 300)
-        assertThat(plan!!.observedPfsDays).isEqualTo(5)
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(treatment, pfsMeasures, response, interval)
+        assertThat(plan!!.observedPfsDays).isEqualTo(17)
         assertThat(plan.hadProgressionEvent).isTrue()
+    }
+
+    @Test
+    fun `Should interpret last pfs measure if there is no measure after treatment end`() {
+        val pfsMeasures = listOf(
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 2),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 6),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 14),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 18),
+        )
+
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(treatment, pfsMeasures, response, interval)
+        assertThat(plan!!.observedPfsDays).isEqualTo(13)
+        assertThat(plan.hadProgressionEvent).isTrue()
+    }
+
+    @Test
+    fun `Should not interpret any pfs measure if there are both censor and progression measures`() {
+        val pfsMeasures = listOf(
+            PfsMeasure(PfsMeasureType.CENSOR, null, 2),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 6),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 14),
+            PfsMeasure(PfsMeasureType.PROGRESSION, null, 18),
+        )
+
+        val plan = NcrSystemicTreatmentPlanExtractor().extractSystemicTreatmentPlan(treatment, pfsMeasures, response, interval)
+        assertThat(plan!!.observedPfsDays).isNull()
+        assertThat(plan.hadProgressionEvent).isNull()
     }
 }
