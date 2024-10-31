eb142359cb0c37d0886d42a93b423309344bcf8b
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-10-31 14:46:31 +0100

    ACTIN-1370: Remove companion object

--- datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/Diagnosis.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/Diagnosis.kt
@@ -44,24 +44,20 @@ data class Diagnosis(
     val hasBrafV600EMutation: Boolean? = null,
     val hasRasMutation: Boolean? = null,
     val hasKrasG12CMutation: Boolean? = null,
-) : Tumor {
+) : Tumor
 
-    companion object {
+private fun determineSidedness(locations: Set<Location>): Sidedness? {
+    val LOCATIONS_INDICATING_LEFT_SIDEDNESS =
+        setOf(Location.FLEXURA_LIENALIS, Location.DESCENDING_COLON, Location.RECTOSIGMOID, Location.SIGMOID_COLON, Location.RECTUM)
+    val LOCATIONS_INDICATING_RIGHT_SIDEDNESS =
+        setOf(Location.APPENDIX, Location.COECUM, Location.ASCENDING_COLON, Location.FLEXURA_HEPATICA)
 
-        private fun determineSidedness(locations: Set<Location>): Sidedness? {
-            val LOCATIONS_INDICATING_LEFT_SIDEDNESS =
-                setOf(Location.FLEXURA_LIENALIS, Location.DESCENDING_COLON, Location.RECTOSIGMOID, Location.SIGMOID_COLON, Location.RECTUM)
-            val LOCATIONS_INDICATING_RIGHT_SIDEDNESS =
-                setOf(Location.APPENDIX, Location.COECUM, Location.ASCENDING_COLON, Location.FLEXURA_HEPATICA)
+    val containsLeft = locations.any { it in LOCATIONS_INDICATING_LEFT_SIDEDNESS }
+    val containsRight = locations.any { it in LOCATIONS_INDICATING_RIGHT_SIDEDNESS }
 
-            val containsLeft = locations.any { it in LOCATIONS_INDICATING_LEFT_SIDEDNESS }
-            val containsRight = locations.any { it in LOCATIONS_INDICATING_RIGHT_SIDEDNESS }
-
-            return when {
-                containsLeft && !containsRight -> Sidedness.LEFT
-                containsRight && !containsLeft -> Sidedness.RIGHT
-                else -> null
-            }
-        }
+    return when {
+        containsLeft && !containsRight -> Sidedness.LEFT
+        containsRight && !containsLeft -> Sidedness.RIGHT
+        else -> null
     }
 }
