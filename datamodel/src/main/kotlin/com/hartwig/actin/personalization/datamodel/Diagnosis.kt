18bfc777f8471b251c07d2db383d08f0acb4ca8a
Nina Jacobs <67696681+ninajacobs@users.noreply.github.com>
2024-10-31 15:00:39 +0100

    ACTIN-1370: Add 'sidedness' variable to Diagnosis (#46)
    * ACTIN-1370: Add 'sidedness' variable to Diagnosis
    
    * ACTIN-1370: Move determination of sidedness to Diagnosis
    
    * ACTIN-1370: Re-move within Diagnosis
    
    * ACTIN-1370: Private fun
    
    * ACTIN-1370: Remove companion object
    
    * ACTIN-1370: Add to view

--- datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/Diagnosis.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/personalization/datamodel/Diagnosis.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.Serializable
 data class Diagnosis(
     override val consolidatedTumorType: TumorType,
     override val tumorLocations: Set<Location>,
+    val sidedness: Sidedness? = determineSidedness(tumorLocations),
     override val hasHadTumorDirectedSystemicTherapy: Boolean,
 
     val ageAtDiagnosis: Int,
@@ -44,3 +45,19 @@ data class Diagnosis(
     val hasRasMutation: Boolean? = null,
     val hasKrasG12CMutation: Boolean? = null,
 ) : Tumor
+
+private fun determineSidedness(locations: Set<Location>): Sidedness? {
+    val LOCATIONS_INDICATING_LEFT_SIDEDNESS =
+        setOf(Location.FLEXURA_LIENALIS, Location.DESCENDING_COLON, Location.RECTOSIGMOID, Location.SIGMOID_COLON, Location.RECTUM)
+    val LOCATIONS_INDICATING_RIGHT_SIDEDNESS =
+        setOf(Location.APPENDIX, Location.COECUM, Location.ASCENDING_COLON, Location.FLEXURA_HEPATICA)
+
+    val containsLeft = locations.any { it in LOCATIONS_INDICATING_LEFT_SIDEDNESS }
+    val containsRight = locations.any { it in LOCATIONS_INDICATING_RIGHT_SIDEDNESS }
+
+    return when {
+        containsLeft && !containsRight -> Sidedness.LEFT
+        containsRight && !containsLeft -> Sidedness.RIGHT
+        else -> null
+    }
+}
