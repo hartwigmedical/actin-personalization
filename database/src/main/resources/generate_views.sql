e1ad318061ea2a5ac4906607a638d63fcba340a4
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-01-14 11:30:47 +0100

    Clean up various views (work in progress)

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -1,86 +1,85 @@
-CREATE OR REPLACE VIEW distantMetastasesOverview
-AS (
+CREATE OR REPLACE VIEW distantMetastasesOverview AS (
+
 SELECT
-    p.ncrId,
-    p.sex,
-    p.isAlive,
-    d.patientId,
-    d.consolidatedTumorType,
-    d.tumorLocations,
-    d.sidedness,
-    d.ageAtDiagnosis,
-    d.observedOsFromTumorIncidenceDays,
-    d.hadSurvivalEvent,
-    d.hasHadPriorTumor,
-    d.isMetachronous,
-    d.cci,
-    d.cciNumberOfCategories,
-    d.cciHasAids,
-    d.cciHasCongestiveHeartFailure,
-    d.cciHasCollagenosis,
-    d.cciHasCopd,
-    d.cciHasCerebrovascularDisease,
-    d.cciHasDementia,
-    d.cciHasDiabetesMellitus,
-    d.cciHasDiabetesMellitusWithEndOrganDamage,
-    d.cciHasOtherMalignancy,
-    d.cciHasOtherMetastaticSolidTumor,
-    d.cciHasMyocardialInfarct,
-    d.cciHasMildLiverDisease,
-    d.cciHasHemiplegiaOrParaplegia,
-    d.cciHasPeripheralVascularDisease,
-    d.cciHasRenalDisease,
-    d.cciHasLiverDisease,
-    d.cciHasUlcerDisease,
-    d.presentedWithIleus,
-    d.presentedWithPerforation,
-    d.anorectalVergeDistanceCategory,
-    d.hasMsi,
-    d.hasBrafMutation,
-    d.hasBrafV600EMutation,
-    d.hasRasMutation,
-    d.hasKrasG12CMutation,
-    e.id as episodeId,
-    e.*,
-    s.surgeries,
-    m.metastasisLocationGroupsPriorToSystemicTreatment,
-    e.intervalTumorIncidenceTreatmentPlanStopDays-e.intervalTumorIncidenceTreatmentPlanStartDays AS systemicTreatmentPlanDuration
-FROM patient p
-JOIN diagnosis d ON p.id = d.patientId
-JOIN episode e ON d.id = e.diagnosisId AND e.order=d.orderOfFirstDistantMetastasesEpisode
-LEFT JOIN (
-    SELECT episodeId, GROUP_CONCAT(type) AS surgeries FROM surgery GROUP BY episodeId
-) s ON e.id = s.episodeId
-LEFT JOIN (
-    SELECT episodeId, GROUP_CONCAT(DISTINCT locationGroup ORDER BY locationGroup) AS metastasisLocationGroupsPriorToSystemicTreatment
-    FROM metastasis mm
-    JOIN episode ee ON mm.episodeId=ee.id
-    WHERE intervalTumorIncidenceMetastasisDetectionDays < intervalTumorIncidenceTreatmentPlanStartDays
-    GROUP BY episodeId
-) m ON e.id=m.episodeId
+    patient.ncrId,
+    patient.sex,
+    patient.isAlive,
+    diagnosis.patientId,
+    diagnosis.consolidatedTumorType,
+    diagnosis.tumorLocations,
+    diagnosis.sidedness,
+    diagnosis.ageAtDiagnosis,
+    diagnosis.observedOsFromTumorIncidenceDays,
+    diagnosis.hadSurvivalEvent,
+    diagnosis.hasHadPriorTumor,
+    diagnosis.isMetachronous,
+    diagnosis.cci,
+    diagnosis.cciNumberOfCategories,
+    diagnosis.cciHasAids,
+    diagnosis.cciHasCongestiveHeartFailure,
+    diagnosis.cciHasCollagenosis,
+    diagnosis.cciHasCopd,
+    diagnosis.cciHasCerebrovascularDisease,
+    diagnosis.cciHasDementia,
+    diagnosis.cciHasDiabetesMellitus,
+    diagnosis.cciHasDiabetesMellitusWithEndOrganDamage,
+    diagnosis.cciHasOtherMalignancy,
+    diagnosis.cciHasOtherMetastaticSolidTumor,
+    diagnosis.cciHasMyocardialInfarct,
+    diagnosis.cciHasMildLiverDisease,
+    diagnosis.cciHasHemiplegiaOrParaplegia,
+    diagnosis.cciHasPeripheralVascularDisease,
+    diagnosis.cciHasRenalDisease,
+    diagnosis.cciHasLiverDisease,
+    diagnosis.cciHasUlcerDisease,
+    diagnosis.presentedWithIleus,
+    diagnosis.presentedWithPerforation,
+    diagnosis.anorectalVergeDistanceCategory,
+    diagnosis.hasMsi,
+    diagnosis.hasBrafMutation,
+    diagnosis.hasBrafV600EMutation,
+    diagnosis.hasRasMutation,
+    diagnosis.hasKrasG12CMutation,
+    episode.id as episodeId,
+    episode.*,
+    surgeryOverview.surgeries,
+    metastasisOverview.metastasisLocationGroupsPriorToSystemicTreatment,
+    episode.intervalTumorIncidenceTreatmentPlanStopDays - episode.intervalTumorIncidenceTreatmentPlanStartDays AS systemicTreatmentPlanDuration
+FROM patient
+    INNER JOIN diagnosis ON patient.id = diagnosis.patientId
+    INNER JOIN episode ON diagnosis.id = episode.diagnosisId AND episode.order = diagnosis.orderOfFirstDistantMetastasesEpisode
+    LEFT JOIN (
+        SELECT episodeId, GROUP_CONCAT(type) AS surgeries FROM surgery GROUP BY episodeId
+    ) surgeryOverview ON episode.id = surgeryOverview.episodeId
+    LEFT JOIN (
+        SELECT episodeId, GROUP_CONCAT(DISTINCT locationGroup ORDER BY locationGroup) AS metastasisLocationGroupsPriorToSystemicTreatment
+        FROM metastasis INNER JOIN episode ON metastasis.episodeId = episode.id
+        WHERE intervalTumorIncidenceMetastasisDetectionDays < intervalTumorIncidenceTreatmentPlanStartDays
+        GROUP BY episodeId
+    ) metastasisOverview ON episode.id = metastasisOverview.episodeId
 );
 
-CREATE OR REPLACE VIEW palliativeIntents
-AS (
+CREATE OR REPLACE VIEW palliativeIntents AS (
+
 SELECT *
 FROM distantMetastasesOverview
 WHERE distantMetastasesDetectionStatus = 'AT_START'
-AND (tnmCM LIKE 'M1%' OR tnmPM LIKE 'M1%' OR stageTNM LIKE 'IV%')
-AND hasHadPreSurgerySystemicChemotherapy = 0
-AND hasHadPostSurgerySystemicChemotherapy = 0
-AND hasHadPreSurgerySystemicTargetedTherapy = 0
-AND hasHadPostSurgerySystemicTargetedTherapy = 0
-AND surgeries IS NULL
-AND gastroenterologyResections = JSON_ARRAY()
-AND metastasesSurgeries = JSON_ARRAY()
-AND radiotherapies = JSON_ARRAY()
-AND metastasesRadiotherapies = JSON_ARRAY()
-AND hasHadHipecTreatment = 0
-AND (hasReceivedTumorDirectedTreatment = 0 OR systemicTreatmentPlan IS NOT NULL)
+    AND (tnmCM LIKE 'M1%' OR tnmPM LIKE 'M1%' OR stageTNM LIKE 'IV%')
+    AND hasHadPreSurgerySystemicChemotherapy = 0
+    AND hasHadPostSurgerySystemicChemotherapy = 0
+    AND hasHadPreSurgerySystemicTargetedTherapy = 0
+    AND hasHadPostSurgerySystemicTargetedTherapy = 0
+    AND surgeries IS NULL
+    AND gastroenterologyResections = JSON_ARRAY()
+    AND metastasesSurgeries = JSON_ARRAY()
+    AND radiotherapies = JSON_ARRAY()
+    AND metastasesRadiotherapies = JSON_ARRAY()
+    AND hasHadHipecTreatment = 0
+    AND (hasReceivedTumorDirectedTreatment = 0 OR systemicTreatmentPlan IS NOT NULL)
 );
 
-CREATE OR REPLACE VIEW knownPalliativeTreatments
-AS (
+CREATE OR REPLACE VIEW knownPalliativeTreatments AS (
+
 SELECT *
 FROM palliativeIntents
 WHERE systemicTreatmentPlan IS NOT NULL AND systemicTreatmentPlan != 'OTHER'
