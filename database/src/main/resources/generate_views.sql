521d106e179da464a72f9d5cabe9e49aa1dd3f56
Hebe Heij <h.heij@hartwigmedicalfoundation.nl>
2025-04-23 11:57:53 +0200

    ACTIN-1656: Change view creation

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -2,12 +2,12 @@ CREATE OR REPLACE VIEW palliativeIntents AS (
 
 SELECT *
 FROM distantMetastasesOverview
-WHERE metastasisBeforeTreatment = 1
+WHERE hasHadPriorSystemicTherapy = 0 
    AND (clinicalTnmM LIKE 'M1%' OR pathologicalTnmM LIKE 'M1%' OR clinicalTumorStage LIKE 'IV%' OR pathologicalTumorStage LIKE 'IV%')
    AND surgeriesPrimary IS NULL
    AND surgeriesMetastatic IS NULL
    AND surgeriesGastroenterology IS NULL
-   AND hadHipec IS NULL
+   AND hadHipec = 0 
    AND radiotherapiesPrimary IS NULL
    AND radiotherapiesMetastatic IS NULL
 );
@@ -16,5 +16,5 @@ CREATE OR REPLACE VIEW knownPalliativeTreatments AS (
 
 SELECT *
 FROM palliativeIntents
-WHERE systemicTreatmentPlan IS NOT NULL AND systemicTreatmentPlan != 'OTHER'
+WHERE treatment IS NOT NULL AND treatment != 'OTHER'
 );
