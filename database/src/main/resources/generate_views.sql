a8b1b043a852bbaa5368c9fd3642a1e06133022c
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-05-20 10:00:51 +0200

    ACTIN-1996: Update views to resemble old views

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -1,20 +1,20 @@
-CREATE OR REPLACE VIEW palliativeIntents AS (
+CREATE OR REPLACE VIEW palliativeReference AS (
 
-SELECT *
-FROM distantMetastasesOverview
-WHERE hasHadPriorSystemicTherapy = 0 
-   AND (clinicalTnmM LIKE 'M1%' OR pathologicalTnmM LIKE 'M1%' OR clinicalTumorStage LIKE 'IV%' OR pathologicalTumorStage LIKE 'IV%')
-   AND surgeriesPrimary IS NULL
-   AND surgeriesMetastatic IS NULL
-   AND surgeriesGastroenterology IS NULL
-   AND hadHipec = 0 
-   AND radiotherapiesPrimary IS NULL
-   AND radiotherapiesMetastatic IS NULL
+    SELECT *
+    FROM reference
+    WHERE hasHadSystemicTreatmentPriorToMetastaticDiagnosis = 0 
+        AND (clinicalTnmM LIKE 'M1%' OR pathologicalTnmM LIKE 'M1%' OR clinicalTumorStage LIKE 'IV%' OR pathologicalTumorStage LIKE 'IV%')
+        AND hasHadPrimarySurgeryAfterMetastaticDiagnosis = 0
+        AND hasHadGastroenterologySurgeryAfterMetastaticDiagnosis = 0
+        AND hasHadHipecAfterMetastaticDiagnosis = 0 
+        AND hasHadPrimaryRadiotherapyAfterMetastaticDiagnosis = 0
+        AND hasHadMetastaticSurgery = 0
+        AND hasHadMetastaticRadiotherapy = 0
 );
 
-CREATE OR REPLACE VIEW knownPalliativeTreatments AS (
+CREATE OR REPLACE VIEW knownPalliativeTreatedReference AS (
 
-SELECT *
-FROM palliativeIntents
-WHERE treatment IS NOT NULL AND treatment != 'OTHER'
+    SELECT *
+    FROM palliativeReference
+    WHERE firstSystemicTreatmentAfterMetastaticDiagnosis IS NOT NULL AND firstSystemicTreatmentAfterMetastaticDiagnosis != 'OTHER' 
 );
