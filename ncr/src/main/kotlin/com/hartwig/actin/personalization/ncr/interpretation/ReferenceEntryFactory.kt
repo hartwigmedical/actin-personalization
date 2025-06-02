b8ea5617a52c0723a48abd7d8dc10bfda8a3995a
Hebe Heij <h.heij@hartwigmedicalfoundation.nl>
2025-06-02 14:33:48 +0200

    ACTIN-1996: Add NcrRecordReliabilityFilter + tests

--- ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/ReferenceEntryFactory.kt
+++ ncr/src/main/kotlin/com/hartwig/actin/personalization/ncr/interpretation/ReferenceEntryFactory.kt
@@ -3,10 +3,10 @@ package com.hartwig.actin.personalization.ncr.interpretation
 import com.hartwig.actin.personalization.datamodel.ReferenceEntry
 import com.hartwig.actin.personalization.ncr.datamodel.NcrRecord
 import com.hartwig.actin.personalization.ncr.interpretation.extraction.NcrReferenceEntryExtractor
+
 import java.util.stream.Collectors
 
 object ReferenceEntryFactory {
-
     fun create(records: List<NcrRecord>): List<ReferenceEntry> {
         return records.groupBy { it.identification.keyNkr }.values.parallelStream()
             .map { patientRecords -> createReferenceEntriesForPatient(patientRecords) }
@@ -16,6 +16,7 @@ object ReferenceEntryFactory {
 
     private fun createReferenceEntriesForPatient(patientRecords: List<NcrRecord>): List<ReferenceEntry> {
         return patientRecords.groupBy { it.identification.keyZid }.values.parallelStream()
+            .filter { tumorRecords -> NcrRecordReliabilityFilter.isReliableTumorRecord(tumorRecords) }
             .map { tumorRecords -> NcrReferenceEntryExtractor.extract(tumorRecords) }
             .collect(Collectors.toList())
     }
