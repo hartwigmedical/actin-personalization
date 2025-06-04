3ff7c93fc1964f50b2f2b0e3698ac4d020f7d0ee
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-06-04 11:53:36 +0200

    ACTIN-1996: Extend frame for running python models in kotlin

--- prediction/src/main/kotlin/com/hartwig/actin/personalization/prediction/PredictionModelRunnerApplication.kt
+++ prediction/src/main/kotlin/com/hartwig/actin/personalization/prediction/PredictionModelRunnerApplication.kt
@@ -1,14 +1,39 @@
 package com.hartwig.actin.personalization.prediction
 
+import ai.djl.repository.zoo.Criteria
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlin.io.path.Path
 
 class PredictionModelRunnerApplication {
 
     private val logger = KotlinLogging.logger {}
-    
+
     fun run() {
-        logger.info { "Running prediction model runner application" }    
+        logger.info { "Running prediction model runner application" }
+
+        val path = Path(System.getProperty("user.home") + "/hmf/tmp/trained_models/treatment_drug/OS_DeepHitModel.pt")
+
+        val criteria = Criteria.builder()
+            .setTypes(PredictorInput::class.java, PredictorOutput::class.java)
+            .optModelPath(path)
+            .build()
+
+        val model = criteria.loadModel()
+        
+        val predictor = model.newPredictor()
+
+        val input = PredictorInput()
+        val output = predictor.predict(input)
+        logger.info { output }
     }
+}
+
+class PredictorInput() {
+
+}
+
+class PredictorOutput() {
+
 }
 
 fun main() {
